5c746f002fc40b2835407af39fbb8d544418420a
Meesum Panjwani <meesumusa@hotmail.com>
2021-08-08 21:41:23 -0400

    fixed routing bug, able to render sign up and login html templates

--- Website/auth.py
+++ Website/auth.py
@@ -5,7 +5,7 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return render_template("Website\Templates\login.html")
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
@@ -13,4 +13,4 @@ def logout():
 
 @auth.route("/sign-up")
 def sign_up():
-    return render_template("Website\Templates\sign_up.html")
+    return render_template("sign_up.html")
